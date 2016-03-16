6f0c40183c306e3202e5b2f4b5576e0c0d06934e
llissery <llissery@gmail.com>
2016-03-16 09:07:31 +0800

    fill background

--- js.js
+++ js.js
@@ -1,3 +1,16 @@
 /**
  * Created by llissery on 2016/3/13.
  */
+
+var background = document.getElementById("bgCanvas"),
+    bgCtx = background.getContext("2d"),
+    width = window.innerWidth,
+    height = window.innerHeight;
+
+//设置bgCtx宽高
+bgCtx.width = background.width = width;
+bgCtx.height = background.height = height;
+
+// 填充背景色
+bgCtx.fillStyle = '#001F4F';
+bgCtx.fillRect(0, 0, width, height);
